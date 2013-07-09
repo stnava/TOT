76a130b5eb2065afeaf70f1b2c5b478e1825ea5e
brian avants <stnava@gmail.com>
2013-07-09 15:47:15 -0700

    WIP: many run cv

--- scripts/tot_predict.R
+++ scripts/tot_predict.R
@@ -8,13 +8,15 @@ print( summary( mdl ) )
 pp<-predict( mdl ) 
 plot( pp ~ ff$ADJ_HOMETotalScore[as.numeric(names(pp))]  )
 
+
 x<-ff
 y<-ff$ADJ_HOMETotalScore
 nfold<-8
 mycode<-c(1:nfold)
-grouping <- sample( rep( mycode , 1000 ) )[1:nrow(ff)]
-ferr<-rep(NA,nfold)
-predicted<-rep(NA,nrow(ff))
+predicted<-rep(0,nrow(ff))
+predictedct<-rep(0,nrow(ff))
+for ( run in 1:100 ) {
+  grouping <- sample( rep( mycode , 1000 ) )[1:nrow(ff)]
 for ( ii in 1:nfold )  {
   x1<-subset( x, subset = ( grouping != ii ) )
   x2<-subset( x, subset = ( grouping == ii ) )
@@ -23,16 +25,17 @@ for ( ii in 1:nfold )  {
   mdl<-lm( ADJ_HOMETotalScore  ~ Cortex +  deepGrayMatter +  whiteMatter + csf +
 GestAge + BMIdel +   MatAgeAtDel , data=x1 ) 
   fitter<-predict( mdl , newdata=x2 )
-  predicted[ grouping == ii ]<-fitter
-  ferr[ii]<-mean( abs(  fitter - y2  ) )
+  predicted[ grouping == ii ]<-predicted[ grouping == ii ]+fitter
+  fitter<-rep( 1 , sum( grouping == ii ) )
+  predictedct[ grouping == ii ]<-predictedct[ grouping == ii ]+fitter
 }
-print( cor.test( predicted, y )  )
-print( mean( ferr , na.rm = T ) )
-crossValHome<-predicted
+}
+crossValHome<-predicted/predictedct
 realHome<-y 
 mdl<-lm( realHome ~ crossValHome )
 tit<-paste("TOT",nfold,"fold cross-validation predicted Home vs real Home")
-fn<-paste(tit,'.pdf',sep='')
+tit2<-paste("figs/TOT",nfold,"fold_cross-validation_predicted_Home_vs_real_Home",sep="_")
+fn<-paste(tit2,'.pdf',sep='')
 pdf(fn)
 visreg(  mdl , main=tit )
 dev.off()

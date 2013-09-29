78a5f4dba55b94162be300a890cf4e098d49268e
brian avants <stnava@gmail.com>
2013-09-28 15:17:19 -1000

    ENH: organize results reporting

--- scripts/tot_sccan.R
+++ scripts/tot_sccan.R
@@ -1,9 +1,9 @@
 library( robust )
-library(visreg)
-library(foreign)
-library(psych)
-library(ANTsR)
-library(e1071)
+library( visreg )
+library( foreign )
+library( psych )
+library( ANTsR )
+library( e1071 )
 ctl<-lmrob.control( "KS2011", max.it = 1000 )
 labelnames<-read.csv('data/labels_surface_DKT31_fullnames.csv')
 ff<-read.csv('data/TOT_Study_Preliminary_Image_Variables.csv')
@@ -20,33 +20,38 @@ ff$BMIdel[ is.na( ff$BMIdel ) ]<-mean( ff$BMIdel,na.rm = T )
 ff$HOMETotalScore[ is.na( ff$HOMETotalScore ) ]<-mean( ff$HOMETotalScore,na.rm = T ) 
 ff$ADJ_HOMETotalScore[ is.na( ff$ADJ_HOMETotalScore ) ]<-mean( ff$ADJ_HOMETotalScore,na.rm = T ) 
 ######### setup study data #########
-np<-5000
-studyhome<-FALSE
+if ( !exists("np") ) np<-5000
+whbrain<-c(13:ncol(ff))
+if ( !exists("studyhome") ) studyhome<-TRUE
 if ( studyhome ) {
   # home variable study
   wh<-c(11)
-  brain<-as.matrix( cbind( ff[ ,c(3,6,7,8,4)]  , ff[ ,13:ncol(ff) ] ) ) # with BMI
+  demog<-as.matrix(  ff[,wh]*(-1))  # just adj_home , negated 
+  brain<-as.matrix( cbind( ff[ ,c(3,4,8)]  , ff[ ,whbrain ] ) ) # with BMI
 } else {
- wh<-c(3,4,6,7,8,11,12)
- brain<-as.matrix( cbind( bvol , ff[ ,13:ncol(ff) ] ) )
+ wh<-c(3,4,8,11)
+ demog<-as.matrix(  ff[,wh] )  # just adj_home
+ brain<-as.matrix( cbind( bvol , ff[ ,whbrain] ) )
 }
-demog<-as.matrix(  ff[,wh] )  # just adj_home
 colnames( demog )<-colnames( ff )[wh]
 brain<-impute(brain)
 ######### setup analysis #########
-nv<-min( 3, ncol(demog) )
+nv<-( ncol(demog) - 1 )
+if ( nv < 1 ) nv<-1
+nv<-2
 sccan<-sparseDecom2( inmatrix=list( demog , brain ), inmask = c( NA , NA ) ,
-  sparseness=c( 0.33, 0.1 ), nvecs=nv, its=30, smooth=0, perms=np, cthresh = c(0, 0), robust=1 )
+  sparseness=c( -0.4 ,  0.1 ), nvecs=nv, its=30, smooth=0, perms=np, cthresh = c(0, 0), robust=1 )
 eps<-1.e-6
 for ( ind in 1:nv ) {
-  print(paste("Sccanvec",ind))
+  print(paste("Sccanvec",ind,"pvalue",sccan$ccasummary[1,ind+1],"corr",sccan$ccasummary[2,ind+1]))
   print( paste( colnames(demog)[ abs(sccan$eig1[,ind]) > eps ] ) )
-  print( ( sccan$eig1[,ind] )[ abs(sccan$eig1[,ind]) > eps] )
+  print( ( sccan$eig1[,ind] )[ abs(sccan$eig1[,ind]) > eps] / sum(  abs(sccan$eig1[,ind])  ) )
   print("Imaging Predictors")
   myanat<-colnames(brain)[ abs(sccan$eig2[,ind]) > eps ]
   wt2<-( sccan$eig2[,ind] )[ abs(sccan$eig2[,ind]) > eps ] 
   for ( j in 1:length(myanat) )  {
     selector<-labelnames$Anatomy[  myanat[j] == labelnames$ID ] 
-    if ( length( selector ) > 0 ) print(  paste( selector , wt2[j] , sum(wt2[1:j])/sum(wt2) ) )else print( paste(  myanat[j] , wt2[j]  , sum(wt2[1:j])/sum(wt2) ) )
+    if ( length( selector ) > 0 ) print(  paste( selector , wt2[j]/sum(wt2)  , sum(wt2[1:j])/sum(wt2) ) )else print( paste(  myanat[j] , wt2[j]/sum(wt2)   , sum(wt2[1:j])/sum(wt2) ) )
   }
 }
+print( p.adjust( sccan$ccasummary[1,2:(ncol(sccan$ccasummary)-1)] , method="BH" ) )

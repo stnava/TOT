1afad67141d29fedc0e7b2d642637fc82b9b50ad
brian avants <stnava@gmail.com>
2013-09-30 12:47:01 -1000

    WIP: isbi update dataset - FA missing for some subjects

--- scripts/tot_sccan.R
+++ scripts/tot_sccan.R
@@ -7,42 +7,40 @@ library( e1071 )
 eps<-1.e-6
 labelnames<-read.csv('data/labels_surface_DKT31_fullnames.csv')
 ff<-read.csv('data/TOT_Study_Preliminary_Image_Variables.csv')
-demog<-data.frame( read.spss('data/TOT_Demographics_MRI_Analyses July2013.sav') )
+demog<-data.frame( read.spss('data/ShortwithHOMENewbornChartReview2013-09-26.sav') )
 names(demog)[1]<-"ID"
 tissue<-read.csv('data/tissue_volume.csv')
 ffmerge<-merge( demog, tissue )  #
 inds<-c(1:nrow(ffmerge))
-bvol<-apply( ffmerge[,13:15], FUN = sum, MARGIN = 1 )
+bvol<-apply( ffmerge[,c(19:21)], FUN = sum, MARGIN = 1 )
+bvol<-c( impute(cbind(bvol,bvol))[,1]) 
 ff<-ffmerge
-ff$ADJ_HOMETotalScore<-ff$ADJ_HOMETotalScore*(-1)
 ####### some imputation ##### 
-ff$HC[ is.na( ff$HC ) ]<-mean( ff$HC,na.rm = T ) 
 ff$BMIprepreg[ is.na( ff$BMIprepreg ) ]<-mean( ff$BMIprepreg,na.rm = T ) 
 ff$BMIdel[ is.na( ff$BMIdel ) ]<-mean( ff$BMIdel,na.rm = T ) 
-ff$HOMETotalScore[ is.na( ff$HOMETotalScore ) ]<-mean( ff$HOMETotalScore,na.rm = T ) 
-ff$ADJ_HOMETotalScore[ is.na( ff$ADJ_HOMETotalScore ) ]<-mean( ff$ADJ_HOMETotalScore,na.rm = T ) 
+ff$ZTotalHOMEADJ<-ff$ZTotalHOMEADJ*(-1)  # negatively correlated with BMI 
 ######### setup study data #########
 if ( !exists("np") ) np<-5000
-whbrain<-c(17,19:ncol(ff))
+whbrain<-c(23,25:ncol(ff))
+rbrain<-impute( ff[ ,whbrain] )
+rbrain<-residuals( lm(   as.matrix(rbrain) ~ bvol  ) )
+brain<-as.matrix( cbind( bvol, rbrain ) )
 if ( !exists("studyhome") ) studyhome<-FALSE
 if ( studyhome ) {
   # home variable study
-  wh<-c(11)
+  wh<-c(18)
   demog<-as.matrix(  ff[,wh] )  # just adj_home , negated 
-  brain<-as.matrix( ff[ ,c(3,4,6,8,whbrain)] ) # with BMI
+  brain<-as.matrix( cbind( ff[ ,c(5, 8, 13 )] , brain ) ) # with BMI
   nv<-1
 } else {
- wh<-c( 3, 4, 6, 8 )
+ wh<-c( 5 , 8, 13, 18 )
  demog<-as.matrix(  ff[,wh] )  # just adj_home
- rbrain<-impute( ff[ ,whbrain] )
- rbrain<-residuals( lm(   as.matrix(rbrain) ~ bvol  ) )
- brain<-as.matrix( cbind(  bvol, rbrain ) )
  nv<-3
 }
 colnames( demog )<-colnames( ff )[wh]
 ######### setup analysis #########
 sccan<-sparseDecom2( inmatrix=list( demog , brain ), inmask = c( NA , NA ) ,
-  sparseness=c( 0.25 ,  0.1 ), nvecs=nv, its=20, smooth=0, perms=np, cthresh = c(0, 0), robust=1 )
+  sparseness=c( 0.15 ,  0.05 ), nvecs=nv, its=15, smooth=0, perms=np, cthresh = c(0, 0), robust=1 )
 for ( ind in 1:nv ) {
   print(paste("Sccanvec",ind,"pvalue",sccan$ccasummary[1,ind+1],"corr",sccan$ccasummary[2,ind+1]))
   print( paste( colnames(demog)[ abs(sccan$eig1[,ind]) > eps ] ) )

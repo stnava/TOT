3021839bbfdcf707adfb5c2d969bfa9ba1b365fc
brian avants <stnava@gmail.com>
2013-09-30 14:58:45 -1000

    WIP: isbi update dataset - subset option

--- scripts/tot_sccan.R
+++ scripts/tot_sccan.R
@@ -7,24 +7,29 @@ library( e1071 )
 eps<-1.e-6
 labelnames<-read.csv('data/labels_surface_DKT31_fullnames.csv')
 ff<-read.csv('data/TOT_Study_Preliminary_Image_Variables.csv')
+demogin<-data.frame( read.spss('data/ShortwithHOMENewbornChartReview2013-09-26.sav') )
 demog<-data.frame( read.spss('data/ShortwithHOMENewbornChartReview2013-09-26.sav') )
 names(demog)[1]<-"ID"
 tissue<-read.csv('data/tissue_volume.csv')
 ffmerge<-merge( demog, tissue )  #
 inds<-c(1:nrow(ffmerge))
 bvol<-apply( ffmerge[,c(19:21)], FUN = sum, MARGIN = 1 )
-bvol<-c( impute(cbind(bvol,bvol))[,1]) 
-ff<-ffmerge
+bvol<-c( impute(cbind(bvol,bvol))[,1])
+initialN<-length(bvol)
+ffmerge<-cbind( ffmerge, bvol ) 
+mysel<-rep( c(1,2) , (length( bvol )/2-1) )
+mysel<-rnorm( length( bvol ) ) < 100.
+ff<-subset( ffmerge , mysel )
 ####### some imputation ##### 
 ff$BMIprepreg[ is.na( ff$BMIprepreg ) ]<-mean( ff$BMIprepreg,na.rm = T ) 
 ff$BMIdel[ is.na( ff$BMIdel ) ]<-mean( ff$BMIdel,na.rm = T ) 
 ff$ZTotalHOMEADJ<-ff$ZTotalHOMEADJ*(-1)  # negatively correlated with BMI 
 ######### setup study data #########
 if ( !exists("np") ) np<-5000
-whbrain<-c(23,25:ncol(ff))
+whbrain<-c(23,25:(ncol(ff)-1))
 rbrain<-impute( ff[ ,whbrain] )
-rbrain<-residuals( lm(   as.matrix(rbrain) ~ bvol  ) )
-brain<-as.matrix( cbind( bvol, rbrain ) )
+rbrain<-residuals( lm(   as.matrix(rbrain) ~ ff$bvol  ) )
+brain<-as.matrix(  cbind( ff$bvol, rbrain  ) )
 if ( !exists("studyhome") ) studyhome<-FALSE
 if ( studyhome ) {
   # home variable study
@@ -40,7 +45,7 @@ if ( studyhome ) {
 colnames( demog )<-colnames( ff )[wh]
 ######### setup analysis #########
 sccan<-sparseDecom2( inmatrix=list( demog , brain ), inmask = c( NA , NA ) ,
-  sparseness=c( 0.25 ,  0.05 ), nvecs=nv, its=15, smooth=0, perms=np, cthresh = c(0, 0), robust=1 )
+  sparseness=c( 0.1 ,  0.05 ), nvecs=nv, its=15, smooth=0, perms=np, cthresh = c(0, 0), robust=1 )
 for ( ind in 1:nv ) {
   print(paste("Sccanvec",ind,"pvalue",sccan$ccasummary[1,ind+1],"corr",sccan$ccasummary[2,ind+1]))
   print( paste( colnames(demog)[ abs(sccan$eig1[,ind]) > eps ] ) )
@@ -55,3 +60,4 @@ for ( ind in 1:nv ) {
 }
 print("Adjusted p-values") 
 print( p.adjust( sccan$ccasummary[1,2:(ncol(sccan$ccasummary)-1)] , method="BH" ) )
+print(sum(as.numeric(mysel))/initialN)

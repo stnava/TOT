34b9c0b135748969a9a62a89f3e338041569adb4
brian avants <stnava@gmail.com>
2013-10-01 15:08:49 -1000

    ENH: home study

--- scripts/tot_sccan.R
+++ scripts/tot_sccan.R
@@ -18,34 +18,24 @@ bvol<-c( impute(cbind(bvol,bvol))[,1])
 initialN<-length(bvol)
 ffmerge<-cbind( ffmerge, bvol ) 
 mysel<-rep( c(1,2) , (length( bvol )/2-1) )
-mysel<-rnorm( length( bvol ) ) < 100.
+mysel<-rnorm( length( bvol ) ) < 1.e6
 ff<-subset( ffmerge , mysel )
 ####### some imputation ##### 
 ff$BMIprepreg[ is.na( ff$BMIprepreg ) ]<-mean( ff$BMIprepreg,na.rm = T ) 
 ff$BMIdel[ is.na( ff$BMIdel ) ]<-mean( ff$BMIdel,na.rm = T ) 
-ff$ZTotalHOMEADJ<-ff$ZTotalHOMEADJ*(-1)  # negatively correlated with BMI 
 ######### setup study data #########
 if ( !exists("np") ) np<-5000
-whbrain<-c(23,25:(ncol(ff)-1))
+whbrain<-c(25:(ncol(ff)-1))
 rbrain<-impute( ff[ ,whbrain] )
-rbrain<-residuals( lm(   as.matrix(rbrain) ~ ff$bvol  ) )
-brain<-as.matrix(  cbind( ff$bvol, rbrain  ) )
-if ( !exists("studyhome") ) studyhome<-FALSE
-if ( studyhome ) {
-  # home variable study
-  wh<-c(18)
-  demog<-as.matrix(  ff[,wh] )  # just adj_home , negated 
-  brain<-as.matrix( cbind( ff[ ,c(5, 8, 10, 13 )] , brain ) ) # with BMI
-  nv<-1
-} else {
- wh<-c( 5 , 8, 13, 18 )
- demog<-as.matrix(  ff[,wh] )  # just adj_home
- nv<-4
-}
+rbrain<-residuals( lm(   as.matrix(rbrain) ~ ff$bvol ) )
+brain<-as.matrix(  data.frame( bvol = ff$bvol, rbrain  ) )
+wh<-c( 5 , 8, 12 , 18 )  # 10 = EGAcalc,  11 = AdmHC
+demog<-as.matrix(  ff[,wh] )  # just adj_home
+nv<-4
 colnames( demog )<-colnames( ff )[wh]
 ######### setup analysis #########
 sccan<-sparseDecom2( inmatrix=list( demog , brain ), inmask = c( NA , NA ) ,
-  sparseness=c( 0.1 ,  0.05 ), nvecs=nv, its=15, smooth=0, perms=np, cthresh = c(0, 0), robust=1 )
+  sparseness=c( 0.1 ,  0.1 ), nvecs=nv, its=55, smooth=0, perms=np, cthresh = c(0, 0), robust=1 )
 for ( ind in 1:nv ) {
   print(paste("Sccanvec",ind,"pvalue",sccan$ccasummary[1,ind+1],"corr",sccan$ccasummary[2,ind+1]))
   print( paste( colnames(demog)[ abs(sccan$eig1[,ind]) > eps ] ) )
@@ -55,9 +45,10 @@ for ( ind in 1:nv ) {
   wt2<-( sccan$eig2[,ind] )[ abs(sccan$eig2[,ind]) > eps ] 
   for ( j in 1:length(myanat) )  {
     selector<-labelnames$Anatomy[  myanat[j] == labelnames$ID ] 
-    if ( length( selector ) > 0 ) print(  paste( selector , wt2[j]/sum(wt2)  , sum(wt2[1:j])/sum(wt2) ) )else print( paste(  myanat[j] , wt2[j]/sum(wt2)   , sum(wt2[1:j])/sum(wt2) ) )
+    if ( length( selector ) > 0 ) print(  paste( selector , wt2[j]/sum(abs(wt2))  , sum(abs(wt2[1:j]))/sum(abs(wt2)) , which( colnames(brain) == myanat[j] ) ) )else print( paste(  myanat[j] , wt2[j]/sum(abs(wt2))  , sum(abs(wt2[1:j]))/sum(abs(wt2)) ) )
   }
 }
 print("Adjusted p-values") 
 print( p.adjust( sccan$ccasummary[1,2:(ncol(sccan$ccasummary)-1)] , method="BH" ) )
 print(sum(as.numeric(mysel))/initialN)
+print( colnames(demog) ) 

47dd1547ddaed6e9a2f330e2e5e6bd73af69ffed
BBAvants <stnava@gmail.com>
2014-05-15 12:30:04 -0400

    ENH: reorganize ---- move MALF to the top => better brain mask?

--- scripts/antsNeo.sh
+++ scripts/antsNeo.sh
@@ -41,21 +41,53 @@ invmap=" -t [${nm}0GenericAffine.mat,1] -t ${nm}1InverseWarp.nii.gz -r $subjecti
 for  x in 1 2 3 4 5 6 ; do 
   antsApplyTransforms -d 3 -i ${templatepriors}${x}.nii.gz -o ${nm}_priors${x}.nii.gz $invmap 
 done
+# initial brain mask --- doesnt have to be perfect
 antsApplyTransforms -d 3 -i ${templatebmask} -o ${nm}_brainmaskt.nii.gz $invmap -n NearestNeighbor
-# segmentation 
+####### get a better brain mask via malf ########
+if [[ ${#malfdir} -gt 3 ]] && [[ ! -s ${nm}_fusionMalfLabels.nii.gz ]] ; then 
+  cts=" 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 "
+  echo "begin malf by building malf label list from training data $cts"
+  segcmd=""
+  for ct in $cts  ; do 
+    segcmd=" $segcmd -g ${malfdir}/${ct}_T2.nii.gz -l  ${malfdir}/${ct}_seg.nii.gz "
+  done 
+  ImageMath 3 ${nm}_temp.nii.gz MD ${nm}_brainmaskt.nii.gz 1
+  MultiplyImages 3 $subjectimage ${nm}_temp.nii.gz ${nm}_brain.nii.gz 
+  antsMalfLabeling.sh -k 0 -c 0 -q 1 \
+    -d 3 \
+    -o ${nm}_fusion \
+    -t ${nm}_brain.nii.gz \
+    $segcmd
+  if [[ -s ${nm}_fusionMalfLabels.nii.gz ]] ; then 
+    ImageMath 3 ${nm}_temp.nii.gz ME ${nm}_brainmaskt.nii.gz 5
+    ThresholdImage 3 ${nm}_fusionMalfLabels.nii.gz ${nm}_brainmask.nii.gz 1 Inf
+    ImageMath 3 ${nm}_brainmask.nii.gz GetLargestComponent ${nm}_brainmask.nii.gz
+    ImageMath 3 ${nm}_brainmask.nii.gz addtozero ${nm}_brainmask.nii.gz ${nm}_temp.nii.gz
+    MultiplyImages 3 $subjectimage ${nm}_brainmask.nii.gz ${nm}_brain.nii.gz 
+    rm ${nm}_temp.nii.gz
+    echo MALF Done!
+  else 
+    echo MALF failed
+  fi
+fi
+
 ImageMath 3 ${nm}_norm.nii.gz TruncateImageIntensity $subjectimage 0.05 0.995 256
 N3BiasFieldCorrection 3 ${nm}_norm.nii.gz ${nm}_norm.nii.gz 8
 N3BiasFieldCorrection 3 ${nm}_norm.nii.gz ${nm}_norm.nii.gz 4
 ImageMath 3 ${nm}_norm.nii.gz Normalize ${nm}_norm.nii.gz
-ThresholdImage 3 ${nm}_norm.nii.gz ${nm}_brainmask.nii.gz 0.3 1
-MultiplyImages 3 ${nm}_brainmask.nii.gz ${nm}_brainmaskt.nii.gz ${nm}_brainmask.nii.gz
-ImageMath 3 ${nm}_brainmaskt.nii.gz ME ${nm}_brainmaskt.nii.gz 10
-ImageMath 3 ${nm}_brainmask.nii.gz + ${nm}_brainmask.nii.gz ${nm}_brainmaskt.nii.gz
-ThresholdImage 3 ${nm}_brainmask.nii.gz ${nm}_brainmask.nii.gz 0.25 Inf
-ImageMath 3 ${nm}_brainmask.nii.gz FillHoles ${nm}_brainmask.nii.gz 
-ImageMath 3 ${nm}_brainmask.nii.gz ME ${nm}_brainmask.nii.gz 2 
-ImageMath 3 ${nm}_brainmask.nii.gz GetLargestComponent ${nm}_brainmask.nii.gz 2 
-ImageMath 3 ${nm}_brainmask.nii.gz MD ${nm}_brainmask.nii.gz 2 
+if  [[ 1 == 0 ]] ; then # old brain masking 
+  ThresholdImage 3 ${nm}_norm.nii.gz ${nm}_brainmask.nii.gz 0.3 1
+  MultiplyImages 3 ${nm}_brainmask.nii.gz ${nm}_brainmaskt.nii.gz ${nm}_brainmask.nii.gz
+  ImageMath 3 ${nm}_brainmaskt.nii.gz ME ${nm}_brainmaskt.nii.gz 10
+  ImageMath 3 ${nm}_brainmask.nii.gz + ${nm}_brainmask.nii.gz ${nm}_brainmaskt.nii.gz
+  ThresholdImage 3 ${nm}_brainmask.nii.gz ${nm}_brainmask.nii.gz 0.25 Inf
+  ImageMath 3 ${nm}_brainmask.nii.gz FillHoles ${nm}_brainmask.nii.gz 
+  ImageMath 3 ${nm}_brainmask.nii.gz ME ${nm}_brainmask.nii.gz 2 
+  ImageMath 3 ${nm}_brainmask.nii.gz GetLargestComponent ${nm}_brainmask.nii.gz 2 
+  ImageMath 3 ${nm}_brainmask.nii.gz MD ${nm}_brainmask.nii.gz 2 
+fi
+
+# segmentation steps below
 ImageMath 3 ${nm}_norm.nii.gz Normalize ${nm}_norm.nii.gz
 antsLaplacianBoundaryCondition.R --output ${nm}_laplacian.nii.gz --mask  ${nm}_brainmask.nii.gz  --input ${nm}_norm.nii.gz 
 t1w=${nm}_t1_normWarped.nii.gz
@@ -65,7 +97,7 @@ if [[ -s $t1 ]] && [[ ${#t1} -gt 3 ]] ; then
   N3BiasFieldCorrection 3 $t1w  $t1w  4
   antsLaplacianBoundaryCondition.R --output ${nm}_laplacian2.nii.gz --mask  ${nm}_brainmask.nii.gz  --input $t1w
   Atropos  -d 3 -x ${nm}_brainmask.nii.gz  -i PriorProbabilityImages[6,${nm}_priors%d.nii.gz,0.25]  -c [50,0] -o [${nm}_t1_seg.nii.gz,${nm}_t1_prob%0d.nii.gz] -m [0.1,1x1x1] -a $t1w -a ${nm}_norm.nii.gz
-  MultiplyImages 3 ${nm}_t1_prob1.nii.gz 0.15 ${nm}_t1_prob1.nii.gz
+  MultiplyImages 3 ${nm}_t1_prob1.nii.gz 0.1 ${nm}_t1_prob1.nii.gz
 fi
 if [[ -s  ${nm}_t1_prob1.nii.gz ]]  ; then 
   ImageMath 3 ${nm}_norm.nii.gz + ${nm}_norm.nii.gz ${nm}_t1_prob1.nii.gz  # increase csf intensity 
@@ -76,9 +108,7 @@ if [[ -s $md ]] && [[ ${#md} -gt 3 ]]  && [[ ${#t1} -lt 3 ]] ; then
   MultiplyImages 3 ${nm}_norm.nii.gz ${nm}_brainmask.nii.gz ${nm}_norm.nii.gz
   antsRegistrationSyNQuick.sh -f ${nm}_norm.nii.gz -m $md -j 0 -o ${nm}_md_norm -t sr
   Atropos  -d 3 -x ${nm}_brainmask.nii.gz  -i kmeans[3] -a $mdw -c [1,0] -o [${nm}_md_seg.nii.gz,${nm}_md_prob%0d.nii.gz]
-  MultiplyImages 3 ${nm}_md_prob3.nii.gz 0.01 ${nm}_temp.nii.gz
   MultiplyImages 3 ${nm}_md_prob3.nii.gz 0.15 ${nm}_md_prob3.nii.gz
-#  MultiplyImages 3 ${nm}_priors3.nii.gz ${nm}_temp.nii.gz ${nm}_priors3.nii.gz  # reduce wm prob here
   ImageMath 3 ${nm}_norm.nii.gz + ${nm}_norm.nii.gz ${nm}_md_prob3.nii.gz  # increase csf intensity 
 fi
 if [[ -s ${nm}_laplacian.nii.gz ]] &&  [[ -s ${nm}_brainmask.nii.gz ]] &&  [[ -s ${nm}_norm.nii.gz ]] ; then
@@ -92,21 +122,6 @@ if [[ ! -s ${nm}LapSegmentation.nii.gz ]] ; then
   antsAtroposN4.sh -d 3 -m 1 -n $atits -x ${nm}_brainmask.nii.gz -c 6 -p ${nm}_priors%d.nii.gz -w 0.25 -o ${nm}Lap         -r "[0.1,1x1x1]" -a ${nm}_norm.nii.gz  -a ${nm}_laplacian.nii.gz 
 fi 
 echo "Finished segmentation"
-if [[ ${#malfdir} -gt 3 ]] && [[ ! -s ${nm}_fusionMalfLabels.nii.gz ]] ; then 
-  cts=" 01 02 03 04 05 06 07 08 09 10 "
-  echo "begin malf by building malf label list from training data $cts"
-  segcmd=""
-  for ct in $cts  ; do 
-    segcmd=" $segcmd -g ${malfdir}/${ct}_T2.nii.gz -l  ${malfdir}/${ct}_seg.nii.gz "
-  done 
-  MultiplyImages 3 ${nm}_norm.nii.gz ${nm}_brainmask.nii.gz ${nm}_brain.nii.gz 
-  antsMalfLabeling.sh -k 0 -c 0 -q 1 \
-    -d 3 \
-    -o ${nm}_fusion \
-    -t ${nm}_brain.nii.gz \
-    $segcmd
-  echo MALF Done!
-fi
 DIRECT=KellyKapowski
 DIRECT_CONVERGENCE="[45,0.0,10]"
 DIRECT_THICKNESS_PRIOR="10"

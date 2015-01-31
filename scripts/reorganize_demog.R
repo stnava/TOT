59527e74a54d733bae9a761e896b7f57943591ba
stnava <stnava@gmail.com>
2015-01-31 13:23:13 -0500

    ENH: dti pro

--- scripts/reorganize_demog.R
+++ scripts/reorganize_demog.R
@@ -68,5 +68,33 @@ summary(lm(  AnatomicalQualityMeasure ~ MomEd + ITNEnrollment,
 
 baddt<-c("7006_20140430_DTIfa.nii.gz","7038_20130904_DTIfa.nii.gz","7041_20140826_DTIfa.nii.gz","","","")
 demogreo$DTQualityMeasure[ !is.na(demogreo$DTFileExtension) ]<-1
-for ( dftn in baddt )
+for ( dtfn in baddt )
   demogreo$DTQualityMeasure[ demogreo$DTFileExtension == dtfn ]<-0
+
+# now we are set up to process dti
+if ( processDT )
+  {
+  library(ANTsR)
+  setwd("/Users/stnava/data/TOT/tempdti")
+  wfn<-na.omit(demogreo$DTFileExtension[ demogreo$DTQualityMeasure == 1])
+  for ( k in 1:length(wfn) )
+    {
+    fafn<-Sys.glob(paste("../data/dti_recon/*/*/*/",wfn[k],sep=''))
+    onm<-paste("w",wfn[k],sep='')
+    if ( ! file.exists(onm) )
+      {
+      fa<-antsImageRead(fafn,3)
+      fam<-getMask(fa,0.02,0.8*mean(fa),2);
+      ImageMath(3,fam,'MD',fam,1)
+      ImageMath(3,fam,'ME',fam,1)
+      ImageMath(3,fam,'FillHoles',fam)
+    #  fa[fam==0]<-0
+      ref<-antsImageRead("fa_ref.nii.gz",3)
+      refm<-antsImageRead("fa_ref_mask.nii.gz",3)
+      mytx<-antsRegistration(fixed=ref , moving=fa ,
+             typeofTransform = c("SyN"),
+             outprefix=tempfile(),mask=refm)
+      antsImageWrite( mytx$warpedmovout , onm  )
+      }
+    }
+  }

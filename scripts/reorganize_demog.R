7463938b8adf9b06d8de7be71807895e0db6e71e
stnava <stnava@gmail.com>
2015-01-31 23:45:58 -0500

    ENH: automate params

--- scripts/reorganize_demog.R
+++ scripts/reorganize_demog.R
@@ -1,3 +1,4 @@
+setwd("/Users/stnava/data/TOT/")
 demog<-read.csv("data/TOT_raw_demog.csv")
 qual<-read.csv("data/TOT_BA_Notes.csv")
 date2ppdformat<-function( x )
@@ -72,11 +73,13 @@ for ( dtfn in baddt )
   demogreo$DTQualityMeasure[ demogreo$DTFileExtension == dtfn ]<-0
 
 # now we are set up to process dti
-if ( processDT )
+if ( TRUE )
   {
   library(ANTsR)
   setwd("/Users/stnava/data/TOT/tempdti")
   wfn<-na.omit(demogreo$DTFileExtension[ demogreo$DTQualityMeasure == 1])
+  ref<-antsImageRead("fa_avg.nii.gz",3)
+  refm<-antsImageRead("fa_ref_mask.nii.gz",3)
   for ( k in 1:length(wfn) )
     {
     fafn<-Sys.glob(paste("../data/dti_recon/*/*/*/",wfn[k],sep=''))
@@ -89,12 +92,56 @@ if ( processDT )
       ImageMath(3,fam,'ME',fam,1)
       ImageMath(3,fam,'FillHoles',fam)
     #  fa[fam==0]<-0
-      ref<-antsImageRead("fa_ref.nii.gz",3)
-      refm<-antsImageRead("fa_ref_mask.nii.gz",3)
       mytx<-antsRegistration(fixed=ref , moving=fa ,
              typeofTransform = c("SyN"),
              outprefix=tempfile(),mask=refm)
       antsImageWrite( mytx$warpedmovout , onm  )
       }
     }
+    if ( FALSE ) {
+      ilist<-imageFileNames2ImageList( Sys.glob("w*gz") , 3 )
+      rad<-rep(3,ref@dimension)
+      pp<-jointIntensityFusion(ref,refm,ilist, rSearch=0, rad=rad )
+    }
+    sel<-demogreo$DTQualityMeasure == 1 & demogreo$Age < 100 &
+      !is.na(demogreo$Age)  & !is.na(demogreo$DTQualityMeasure )
+    babylist<-paste("w",na.omit( demogreo$DTFileExtension[sel] ), sep='' )
+    ref[  refm == 0  ]<-0
+    famask<-antsImageRead('famask_topo_skel.nii.gz',3)
+    mat<-imagesToMatrix( babylist , famask )
+    subdemog<-demogreo[sel,]
+    confinds<-c(5,7,8,38,40)
+    confoundmat<-antsrimpute(data.matrix(subdemog[,confinds]))
+    rmat<-residuals(lm(mat~confoundmat))
+    predmat<-antsrimpute(data.matrix(subdemog[,c(10,12,24)]))
+    predmat<-antsrimpute(data.matrix(subdemog[,c(12)]))
+    mat2<-antsrimpute(data.matrix(subdemog[,c(10,12,24,5,6,38,40)]))
+#    sccan<-sparseDecom2( inmatrix=list( rmat, predmat),
+#      inmask=c(famask,NA), nvecs=5, sparseness=c( 0.001, -0.5 ),
+#      cthresh=c(250,0), its=5, mycoption=1, perms=5, smooth=1 )
+    eanat<-sparseDecom( inmatrix=mat, inmask=famask, nvecs=50,
+      sparseness=0.01, cthresh=50000, its=5, mycoption=0 )
+    jeanat<-joinEigenanatomy( mat , famask, eanat$eig,
+      c(0.05,0.075,0.1,0.125,0.15,0.2))
+    #,   joinMethod='multilevel' )
+    useeig<-eanat$eig
+    useeig<-jeanat$fusedlist
+    eseg<-eigSeg( famask, useeig )
+    antsImageWrite( eseg, 'eseg.nii.gz' )
+    avgmat<-abs(imageListToMatrix( useeig , famask ))
+    avgmat<-avgmat/rowSums(abs(avgmat))
+    avgmat<- mat %*% t(avgmat)
+    mylm<-(lm( avgmat ~   .  , data=subdemog[,c(12,confinds)]  ))
+    mylmres<-bigLMStats(mylm)
+    qv<-p.adjust( mylmres$beta.p["ITNEnrollment",][] ,method='holm' )
+    sigct<-1
+    esegq<-antsImageClone( eseg )
+    for ( k in as.numeric( which(qv <= 0.05 ) ) )
+      {
+      esegq[ eseg != k & eseg > sigct ]<-0
+      esegq[ eseg == k ]<-sigct
+      sigct<-sigct+1
+      }
+    antsImageWrite( esegq, 'esegq.nii.gz' )
+    summary(lm( avgmat[,qv<=0.05] ~   .  , data=subdemog[,c(12,confinds)]  ))
   }

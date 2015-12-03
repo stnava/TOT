fe81c69f4e912e265772f2a0e1324a0366be9f1f
stnava <stnava@gmail.com>
2015-12-03 12:18:44 -0500

    DOC: older version

--- scripts/antsNeoLong2.sh
+++ scripts/antsNeoLong2.sh
@@ -1,13 +1,9 @@
 #  registration
 rootdir=$PWD
-templatedir=${rootdir}/template_new/
-template=${templatedir}TOT_template.nii.gz
-templatepriors=${templatedir}PriorsXmodCSF/priors
-templatepriors=${templatedir}PriorsReflect/priors
-templatepriors=${templatedir}PriorsX/priors
-templatebmask=${templatedir}TOT_template_mask.nii.gz
-template=${templatedir}local_17_template.nii.gz
-templatebmask=${templatedir}local_17_template_brain_mask.nii.gz
+templatedir=${rootdir}/template_BA/
+templatepriors=${templatedir}Priors/priors
+template=${templatedir}template_JLF.nii.gz
+templatebmask=${templatedir}template_JLF_brain_mask.nii.gz
 subjectimage=$1
 outdir=$2
 dti=$3
@@ -40,15 +36,15 @@ fi
 if [[ ! -s ${nm}jacobian.nii.gz ]] ; then
   CreateJacobianDeterminantImage 3 ${nm}1Warp.nii.gz ${nm}jacobian.nii.gz 0 1
 fi
-for  x in 1 2 3 4 5 6 ; do
+for  x in 1 2 3 4 5 6 7 ; do # csf gm wm dgm bstem cerebellum ventricles
   antsApplyTransforms -d 3 -i ${templatepriors}${x}.nii.gz -o ${nm}_priors${x}.nii.gz $invmap
 done
 # initial brain mask --- doesnt have to be perfect
 antsApplyTransforms -d 3 -i ${templatebmask} -o ${nm}_brainmaskt.nii.gz $invmap -n NearestNeighbor
 echo got mask
 ####### get a better brain mask via malf ########
-if [[ ${#malfdir} -gt 3 ]] && [[ ! -s ${nm}_fusionMalfLabels.nii.gz ]] ; then
-  cts=" 01 02 03 04 05 06 07 08 09 10 " # 11 12 13 14 15 16 17 18 19 20 " # can cause memory problems
+if [[ ${#malfdir} -gt 3 ]] && [[ ! -s ${nm}_fusionLabels.nii.gz ]] ; then
+  cts=" 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 " # can cause memory problems
   echo "begin malf by building malf label list from training data $cts"
   segcmd=""
   for ct in $cts  ; do
@@ -57,20 +53,19 @@ if [[ ${#malfdir} -gt 3 ]] && [[ ! -s ${nm}_fusionMalfLabels.nii.gz ]] ; then
   ImageMath 3 ${nm}_temp.nii.gz MD ${nm}_brainmaskt.nii.gz 1
   MultiplyImages 3 $subjectimage ${nm}_temp.nii.gz ${nm}_brain.nii.gz
   ImageMath 3 ${nm}_brain.nii.gz ClosestSimplifiedHeaderMatrix ${nm}_brain.nii.gz
-  antsMalfLabeling.sh -m 0 -k 1 -c 0 -q 0  -j $ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS \
-    -d 3 \
+  antsJointLabelFusion.sh -d 3 -k 0 -c 0  -j 1 -q 1 \
     -o ${nm}_fusion \
     -t ${nm}_brain.nii.gz \
     $segcmd
-    echo MALF Done!
+  echo MALF Done!
 fi
 
-if [[ -s ${nm}_fusionMalfLabels.nii.gz ]] ; then
+if [[ -s ${nm}_fusionLabels.nii.gz ]] ; then
     ImageMath 3 ${nm}_norm.nii.gz TruncateImageIntensity $subjectimage 0.005 0.995 256
     ImageMath 3 ${nm}_norm.nii.gz Normalize ${nm}_norm.nii.gz
-    CopyImageHeaderInformation $subjectimage ${nm}_fusionMalfLabels.nii.gz ${nm}_fusionMalfLabels.nii.gz 1 1 1
+    CopyImageHeaderInformation $subjectimage ${nm}_fusionLabels.nii.gz ${nm}_fusionLabels.nii.gz 1 1 1
     ImageMath 3 ${nm}_temp.nii.gz MD ${nm}_brainmaskt.nii.gz 0
-    ThresholdImage 3 ${nm}_fusionMalfLabels.nii.gz ${nm}_brainmask.nii.gz 1 Inf
+    ThresholdImage 3 ${nm}_fusionLabels.nii.gz ${nm}_brainmask.nii.gz 1 Inf
     ImageMath 3 ${nm}_brainmask.nii.gz ME ${nm}_brainmask.nii.gz 8
     ImageMath 3 ${nm}_brainmask.nii.gz GetLargestComponent ${nm}_brainmask.nii.gz
     ImageMath 3 ${nm}_brainmask.nii.gz MD ${nm}_brainmask.nii.gz 9
@@ -81,6 +76,7 @@ if [[ -s ${nm}_fusionMalfLabels.nii.gz ]] ; then
     ThresholdImage 3 ${nm}_otsu.nii.gz ${nm}_otsu.nii.gz 2 3
     MultiplyImages 3 ${nm}_brainmask.nii.gz ${nm}_otsu.nii.gz ${nm}_brainmask.nii.gz
     MultiplyImages 3 ${nm}_norm.nii.gz  ${nm}_brainmask.nii.gz ${nm}_brain.nii.gz
+    ImageMath 3 ${nm}_fusionLabels.csv LabelStats ${nm}_fusionLabels.nii.gz ${nm}_brainmask.nii.gz
 else
     echo MALF failed    exit
 fi
@@ -100,30 +96,25 @@ else
   echo should have produced ${nm}_norm.nii.gz ${nm}_brainmask.nii.gz ${nm}_laplacian.nii.gz
   exit
 fi
-############ dti ###########
-  dtiterm=" "
-  if [[ -s $dti ]] && [[ -s $fa ]] ; then
-    dtiterm=" -a $fa "
-    antsApplyTransforms -d 3 -i $fa -o ${nm}_FractionalAnisotropyToTemplate.nii.gz $fwdmap
-  fi
-  echo DTITERM $dtiterm
- # get ventricle segmentation
-  ThresholdImage 3 ${nm}_fusionMalfLabels.nii.gz  ${nm}_ventmask.nii.gz  49 50
-  ImageMath 3 ${nm}_negventmask.nii.gz Neg ${nm}_ventmask.nii.gz
-  MultiplyImages 3 ${nm}_brainmask.nii.gz   ${nm}_negventmask.nii.gz  ${nm}_segmask.nii.gz
-#  if [[ ! -s ${nm}LapSegmentation.nii.gz ]] ; then
-    antsAtroposN4.sh -d 3 -m 1 -n $atits -x ${nm}_segmask.nii.gz -c 6 -p ${nm}_priors%d.nii.gz -w 0.25 -o ${nm}Lap         -r "[0.1,1x1x1]" -a ${nm}_norm.nii.gz   -a ${nm}_laplacian.nii.gz
-  ImageMath 3  ${nm}LapSegmentation.nii.gz + ${nm}LapSegmentation.nii.gz ${nm}_ventmask.nii.gz
-#  fi
 
+# map jlf segmentation to probabilities
+ThresholdImage 3 ${nm}_fusionLabels.nii.gz  ${nm}_priors4.nii.gz  40 47
+ThresholdImage 3 ${nm}_fusionLabels.nii.gz  ${nm}_priors6.nii.gz  17 18
+ThresholdImage 3 ${nm}_fusionLabels.nii.gz  ${nm}_priors7.nii.gz  49 50
+for x in 4 6 7 ; do
+  SmoothImage 3 ${nm}_priors${x}.nii.gz 0.5 ${nm}_priors${x}.nii.gz
+done
+antsAtroposN4.sh -d 3 -m 1 -n $atits -x ${nm}_segmask.nii.gz -c 7 \
+  -p ${nm}_priors%d.nii.gz -w 0.25 -o ${nm}Lap         \
+  -r "[0.1,1x1x1]" -a ${nm}_norm.nii.gz   -a ${nm}_laplacian.nii.gz
+
+# use geometry to infer sulcal locations
 if [[ -s ${nm}LapSegmentation.nii.gz ]] ; then
   bash ${rootdir}/thk_example.sh ${nm}
   antsApplyTransforms -d 3 -i ${nm}thicknessjj.nii.gz -o ${nm}_CortVolToTemplate.nii.gz $fwdmap
 fi
 if [[ -s ${nm}thicknessjj.nii.gz  ]] ; then
-# ThresholdImage 3 ${nm}LapSegmentation.nii.gz ${nm}wm.nii.gz 3 3
-# ImageMath 3 ${nm}wm.nii.gz GetLargestComponent ${outpre}wm.nii.gz
-for  x in 1 2 3 4 5 6 ; do
+for  x in 1 2 3 5 ; do # exclude the JLF probabilities
   antsApplyTransforms -d 3 -i ${templatepriors}${x}.nii.gz -o ${nm}_priors${x}.nii.gz $invmap
 done
 MultiplyImages 3 ${nm}wm.nii.gz ${nm}_priors3.nii.gz  ${nm}_priors3.nii.gz
@@ -137,11 +128,17 @@ ImageMath 3  ${nm}thicknessjj_neg.nii.gz  Neg  ${nm}thicknessjjsig.nii.gz
 MultiplyImages 3 ${nm}thicknessjj_neg.nii.gz  ${nm}_priors1.nii.gz ${nm}_priors1.nii.gz
 ImageMath 3 ${nm}_priors1.nii.gz + ${nm}thicknessjjcsf.nii.gz ${nm}_priors1.nii.gz
 MultiplyImages 3 ${nm}thicknessjj_neg.nii.gz  ${nm}_priors3.nii.gz ${nm}_priors3.nii.gz
-antsAtroposN4.sh -d 3 -m 1 -n $atits -x ${nm}_segmask.nii.gz -c 6 -p ${nm}_priors%d.nii.gz -w 0.25 -o ${nm}DIFF        -r "[0.1,1x1x1]" -a ${nm}_norm.nii.gz #  $dtiterm  -a ${nm}_laplacian.nii.gz
-ImageMath 3  ${nm}DIFFSegmentation.nii.gz + ${nm}DIFFSegmentation.nii.gz ${nm}_ventmask.nii.gz
+antsAtroposN4.sh -d 3 -m 1 -n $atits -x ${nm}_segmask.nii.gz -c 7 \
+  -p ${nm}_priors%d.nii.gz -w 0.25 -o ${nm}DIFF \
+  -r "[0.1,1x1x1]" -a ${nm}_norm.nii.gz
 fi
 
-echo "Finished segmentation"
+
+########################################################################
+########################################################################
+echo "Finished segmentation 0 now thickness"
+########################################################################
+########################################################################
 DIRECT=KellyKapowski
 DIRECT_CONVERGENCE="[45,0.0,10]"
 DIRECT_THICKNESS_PRIOR="10"
@@ -149,8 +146,8 @@ DIRECT_GRAD_STEP_SIZE="0.02"
 DIRECT_SMOOTHING_SIGMA="1.5"
 DIRECT_NUMBER_OF_DIFF_COMPOSITIONS="10"
 dimension=3
-exe_direct="$DIRECT -d $dimension -s [${nm}LapSegmentation.nii.gz,2,3]"
-tissueprobs=" -g ${nm}LapSegmentationPosteriors2.nii.gz -w ${nm}LapSegmentationPosteriors3.nii.gz "
+exe_direct="$DIRECT -d $dimension -s [${nm}DIFFSegmentation.nii.gz,2,3]"
+tissueprobs=" -g ${nm}DIFFSegmentationPosteriors2.nii.gz -w ${nm}DIFFSegmentationPosteriors3.nii.gz "
 exe_direct="${exe_direct} $tissueprobs -o ${nm}Thickness.nii.gz"
 exe_direct="${exe_direct} -c ${DIRECT_CONVERGENCE} -r ${DIRECT_GRAD_STEP_SIZE}"
 exe_direct="${exe_direct} -m ${DIRECT_SMOOTHING_SIGMA} -n ${DIRECT_NUMBER_OF_DIFF_COMPOSITIONS}"
@@ -158,13 +155,18 @@ exe_direct="${exe_direct} -m ${DIRECT_SMOOTHING_SIGMA} -n ${DIRECT_NUMBER_OF_DIF
   $exe_direct
 # fi
 antsApplyTransforms -d 3 -i ${nm}Thickness.nii.gz -o ${nm}_ThicknessToTemplate.nii.gz $fwdmap
-echo "Finished Thickness"
-# get csv files
-ImageMath 3 ${nm}_TissueSegmentation.csv LabelStats ${nm}LapSegmentation.nii.gz ${nm}_brainmask.nii.gz
-ThresholdImage 3 ${nm}LapSegmentation.nii.gz ${nm}_fusionMalfLabelsBin.nii.gz  1 Inf
-ImageMath 3 ${nm}_Malf.csv LabelStats ${nm}_fusionMalfLabels.nii.gz ${nm}_fusionMalfLabelsBin.nii.gz
-ThresholdImage 3 ${nm}LapSegmentation.nii.gz ${nm}_fusionMalfLabelsGMBin.nii.gz  2 2
-MultiplyImages 3 ${nm}_fusionMalfLabelsGMBin.nii.gz ${nm}_fusionMalfLabels.nii.gz ${nm}_fusionMalfLabelsGM.nii.gz
-ImageMath 3 ${nm}_MalfGM.csv LabelStats ${nm}_fusionMalfLabelsGM.nii.gz ${nm}_fusionMalfLabelsGMBin.nii.gz
-ImageMath 3 ${nm}_MalfThickness.csv LabelStats ${nm}_fusionMalfLabelsGM.nii.gz ${nm}Thickness.nii.gz
+
+########################################################################
+########################################################################
+echo " get csv files "
+########################################################################
+########################################################################
+ImageMath 3 ${nm}_TissueSegmentationDiffeo.csv LabelStats ${nm}DIFFSegmentation.nii.gz ${nm}_brainmask.nii.gz
+ImageMath 3 ${nm}_TissueSegmentation.csv LabelStats ${nm}DIFFSegmentation.nii.gz ${nm}_brainmask.nii.gz
+ThresholdImage 3 ${nm}DIFFSegmentation.nii.gz ${nm}_fusionLabelsBin.nii.gz  1 Inf
+ImageMath 3 ${nm}_Malf.csv LabelStats ${nm}_fusionLabels.nii.gz ${nm}_fusionLabelsBin.nii.gz
+ThresholdImage 3 ${nm}DIFFSegmentation.nii.gz ${nm}_fusionLabelsGMBin.nii.gz  2 2
+MultiplyImages 3 ${nm}_fusionLabelsGMBin.nii.gz ${nm}_fusionLabels.nii.gz ${nm}_fusionLabelsGM.nii.gz
+ImageMath 3 ${nm}_MalfGM.csv LabelStats ${nm}_fusionLabelsGM.nii.gz ${nm}_fusionLabelsGMBin.nii.gz
+ImageMath 3 ${nm}_MalfThickness.csv LabelStats ${nm}_fusionLabelsGM.nii.gz ${nm}Thickness.nii.gz
 echo "Done!"
